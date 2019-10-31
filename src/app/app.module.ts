9495dfdadb22d6ccea5f66185f699f9159da6f97
DacNguyen89 <dac.ngm89@gmail.com>
2019-10-31 16:28:40 +0700

    Generate youtube-playlist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {YoutubePlaylistComponent} from './youtube-playlist/youtube-playlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YoutubePlaylistComponent
   ],
   imports: [
     BrowserModule,
